a8163ac6812a47cbe04991993b3b3ececb2c9472
EduardoEfihm <eduardoogrand@gmail.com>
2026-09-04 22:19:38 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/eduardoefihm/workshopmongo/resources/UserResource.java
+++ src/main/java/com/eduardoefihm/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.eduardoefihm.workshopmongo.domain.Post;
 import com.eduardoefihm.workshopmongo.domain.User;
 import com.eduardoefihm.workshopmongo.dto.UserDTO;
 import com.eduardoefihm.workshopmongo.services.UserService;
@@ -67,5 +68,10 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 
-	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body (obj.getPosts());
+	}
 }
