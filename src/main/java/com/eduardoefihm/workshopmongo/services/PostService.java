2f8d62e0d51874ceeb8cd0c8761634c2d654e335
EduardoEfihm <eduardoogrand@gmail.com>
2026-09-04 22:24:02 -0300

    Obtendo um post por id

---
+++ src/main/java/com/eduardoefihm/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.eduardoefihm.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardoefihm.workshopmongo.domain.Post;
+import com.eduardoefihm.workshopmongo.repository.PostRepository;
+import com.eduardoefihm.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+		public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	
+}
